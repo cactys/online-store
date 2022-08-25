fb72703f0720e2212baf2cd681069270cd2718c2
cactys95 <cactys95@yandex.ru>
2022-08-25 12:07:18 +0300

    feat:

---
+++ client/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <div className="px-3 py-2 bg-dark text-white">
+      <Container>
+        <Row className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="currentColor"
+            class="bi bi-activity"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"
+            />
+          </svg>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
