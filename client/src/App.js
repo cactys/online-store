57d68821265dd5e46bac56f60cd9c9e4fc1b509b
cactys95 <cactys95@yandex.ru>
2022-09-05 23:12:52 +0300

    feat: https://youtu.be/H2GCkRF9eko?t=7329

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
-import Header from './components/Header';
 import NavBar from './components/NavBar';
 
 function App() {
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
 import TypeBar from '../components/TypeBar';
