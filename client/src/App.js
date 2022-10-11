727b5690b7e5eca06a4fb15247f12372753fd85c
cactys95 <cactys95@yandex.ru>
2022-10-11 14:38:13 +0300

    fix: sdfdsfgsdfbsdfb

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
+import Header from './components/Header';
 import NavBar from './components/NavBar';
 
 function App() {
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,54 +5,28 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import {
-  ADMIN_ROUTE,
-  LOGIN_ROUTE,
-  REGISTRATION_ROUTE,
-  SHOP_ROUTE,
-} from '../utils/consts';
+import { LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite';
-import { useHistory } from 'react-router-dom';
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
-  const history = useHistory();
-
   return (
-    <Navbar bg='primary' variant='primary'>
+    <Navbar bg="primary" variant="primary">
       <Container>
         <NavLink style={{ color: 'white', textDecoration: 'none' }} to={SHOP_ROUTE}>
           купиДевайс
         </NavLink>
         {user.isAuth ? (
-          <Nav className='ml-auto' stype={{ color: 'whie' }}>
-            <Button
-              variant='outline-light'
-              onClick={() => history.push(ADMIN_ROUTE)}
-            >
-              Админ панель
-            </Button>
-            <Button
-              variant='outline-light'
-              onClick={() => history.push(LOGIN_ROUTE)}
-              className='ms-2'
-            >
+          <Nav className="ml-auto" stype={{ color: 'whie' }}>
+            <Button variant="outline-light">Админ панель</Button>
+            <Button variant="outline-light" className="ms-2">
               Выйти
             </Button>
           </Nav>
         ) : (
-          <Nav className='ml-auto' stype={{ color: 'whie' }}>
-            <Button
-              variant='outline-light'
-              onClick={() => history.push(LOGIN_ROUTE)}
-            >
-              Авторизация
-            </Button>
-            <Button
-              variant='outline-light'
-              onClick={() => history.push(REGISTRATION_ROUTE)}
-              className='ms-2'
-            >
+          <Nav className="ml-auto" stype={{ color: 'whie' }}>
+            <Button variant="outline-light">Авторизация</Button>
+            <Button variant="outline-light" className="ms-2">
               Регистрация
             </Button>
           </Nav>
